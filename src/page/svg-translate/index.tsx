9f1501e072143dac3243fe36a564f591e8a43ae2
keepsaunter <2336990515@qq.com>
2023-01-08 22:32:09 +0800

    feat: add wheel event

--- src/page/svg-translate/index.tsx
+++ src/page/svg-translate/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import SvgComponent from './svg-component';
+import { addScrollToSvgElement } from 'svg-translate-utils';
 import './index.less';
 
 const PageSvgTranslate: React.FC<any> = () => {
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (svgRef.current) {
+      addScrollToSvgElement(svgRef.current);
+    }
+  }, []);
+
   return (
     <div className='svg-container'>
-      <SvgComponent />
+      <SvgComponent ref={svgRef} />
     </div>
   )
 };
